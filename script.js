03fe45df114e565676a3c68577c99e8c7dd092ef
Ginevra Testa <ginevra99testaa@gmail.com>
2023-10-10 14:23:53 +0200

    last version

---
+++ script.js
@@ -0,0 +1,19 @@
+//Esercitazione nell'uso delle variabili e di come dichiararle - es. per casa.
+
+console.log("Cos'é un algoritmo?");
+let algoritmoDefinizione = "L'algoritmo è un insieme di regole che permettono l'esecuzione di una task o la risoluzione di un determinato problema";
+console.log(algoritmoDefinizione);
+
+
+console.log("Cos'é una variabile?");
+let variabileDefinizione = "Una variabile è un contenitore per dati parzialmente elaborati, altrimenti chiamati input";
+console.log(variabileDefinizione);
+
+
+console.log("Qual è la differenza tra undefined e null?");
+console.log("Definizione di undefined")
+let undefinedDefinizione = "Si tratta di un valore che fa presente, all'interno del terminale, che la variabile non sia dichiarata o non sia dichiarata correttamente"
+console.log(undefinedDefinizione)
+console.log("Definizione di null")
+let nullDefinizione = "Si tratta di un valore impostato volontariamente alla variabile per renderla vuota"
+console.log(nullDefinizione)
